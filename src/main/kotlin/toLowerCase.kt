ab83c159f117bbce3648029dee37f92b79d3a585
Erkan Çalışkan <146715229+erkan011@users.noreply.github.com>
2024-04-22 20:44:18 +0300

    toLowerCase fonksiyonu, bir karakter dizisindeki tüm harfleri küçük harfe dönüştüren bir Kotlin String fonksiyonudur. Bu fonksiyon, metin işlemlerinde büyük-küçük harf duyarlılığını dikkate almak veya belirli formatlara uygunluk sağlamak için kullanılır.

---
+++ src/main/kotlin/toLowerCase.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val originalString = "MERHABA DÜNYA"
+    val lowerCaseString = originalString.toLowerCase()
+
+    println("original metin: $originalString")
+    println("küçük: $lowerCaseString")
+}
