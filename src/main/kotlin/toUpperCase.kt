50f308bf4c00e3c256d9e068df69c37f7c8a9919
Erkan Çalışkan <146715229+erkan011@users.noreply.github.com>
2024-04-22 19:10:55 +0300

    toUpperCase fonksiyonu, bir karakter dizisindeki tüm harfleri büyük harfe dönüştüren bir Kotlin String fonksiyonudur. Bu fonksiyon, genellikle kullanıcı girişi gibi verileri normalleştirmek veya metinsel işlemler yaparken büyük-küçük harf farkını dikkate almak için kullanılır.

---
+++ src/main/kotlin/toUpperCase.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val originalString = "Merhaba erkan"
+    val upperCaseString = originalString.toUpperCase()
+    println("Origin Metin: $originalString")
+    println("Güncel Metin: $upperCaseString")
+}
